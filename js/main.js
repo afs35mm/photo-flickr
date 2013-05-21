c54dd7f4bb20cbe20ed8e63619bfeeef567108ea
AS, akqa <afs35mm@gmail.com>
2013-05-21 10:02:39 -0400

    added pushstate attempt1

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ Flick.App = (function(){
 		secret = '401e034e50dea749',
 		getPhotos = 'flickr.photosets.getPhoto',
 		baseUrl = 'http://api.flickr.com/services/rest/?method=',
-		endUrl = '&api_key=' + key + '&user_id=' + user + '&format=json&jsoncallback=?',
-		setID =null;
+		endUrl = '&api_key=' + key + '&user_id=' + user + '&format=json&jsoncallback=?';
+		//setID =null;
 		
 		var setsUrl = baseUrl + 'flickr.photosets.getList&api_key=' + key + '&user_id=' + user + '&format=json&jsoncallback=?';
 		
@@ -21,23 +21,29 @@ Flick.App = (function(){
 		});
 	};
 
-	var sayHay = function(){
-		alert('say Haaaay');
-	}
 
 	var bindDOMevents = function(){
 		$('ul#nav').on('click', 'li a',function(e){
 			e.preventDefault();
-			setID =  $(this).attr('id');
-			loadPics();
+			
+			var setID =  $(this).attr('id');
+			loadPics( setID );
+		
+			var setName =  $(this).children('span').text() ;		
+			changeURL(setName);
 		});
+
 	};
 
-	var loadPics = function(){
+	var loadPics = function(set){
 		$('#main').empty();
-		var photosUrl = baseUrl + 'flickr.photosets.getPhotos&api_key=' + key + '&photoset_id=' + setID + '&format=json&jsoncallback=?';
-			
-			loadTemplate( 'pics', photosUrl, '#main');
+		var photosUrl = baseUrl + 'flickr.photosets.getPhotos&api_key=' + key + '&photoset_id=' + set + '&format=json&jsoncallback=?';	
+		loadTemplate( 'pics', photosUrl, '#main');
+	};
+
+	var changeURL = function(newUrl){
+		newUrl = newUrl.replace(/\s+/g, '-').toLowerCase();
+		history.pushState(null, null, newUrl)
 	};
 
 	var loadTemplate = function( name, url, divAppendTo, callback ){
@@ -61,3 +67,4 @@ Flick.App = (function(){
 $(document).ready(function(){
 	Flick.App.init();
 });
+
