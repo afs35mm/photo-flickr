2a720155cac7faae3323d2721f6e7ed2d7181d09
AS, akqa <afs35mm@gmail.com>
2013-06-03 17:32:26 -0400

    getting slideshow to work

--- js/EzFade.js
+++ js/EzFade.js
@@ -1,41 +1,67 @@
 (function($) {
     $.fn.EzFade = function(options) {
-    	var settings = $.extend({
-            transition : 'fade',
+        var settings = $.extend({
             duration: 3000,
-            parentName: 'superFade',
-            elmName: 'superFadeElm',
-            fadeSpeed: 1000
+            parentName: 'EzFade',
+            elmName: 'EzFadeElm',
+            fadeSpeed: 1000,
+            width: '100%',
+            height: '100%',
+            position: 'relative'
         }, options);        
-        var self = $(this);
-        $(this).find('img').last().load(function(){           
-           self.addClass(settings.parentName).children(":first").css('display','inline');
-           self.each( function() {
-                var container = $(this),
-                    containerHeight = container.height(),
-                    containerWidth = container.width(),
-                    ratio = containerWidth / containerHeight;
-                container.children().each(function(){
-                    var elm = $(this),
-                        elmRatio = elm.width() / elm.height();
-                    elm.addClass(settings.elmName);   
-                    if( ratio < elmRatio){
-                        elm.css('height', containerHeight)
-                        .css( 'left', -(elm.width() - containerWidth) / 2 );
-                    }else if (ratio > elmRatio ){
-                       elm.css('width', containerWidth)
-                       .css( 'top', -(elm.height() - containerHeight) / 2 );
+        $(this).each(function(){
+            var self = $(this),
+                elmID = self.attr('id');
+            self.css({
+                'width' : settings.width,
+                'height' : settings.height,
+                'position': settings.position, 
+                'overflow': 'hidden'
+            }).addClass(settings.parentName);
+            var containerHeight = self.height(),
+                containerWidth = self.width();
+                ratio = containerWidth / containerHeight;
+            self.children().each(function(pos,value){
+                $(this).load(function(){
+                    var elm = $(this);
+                        var elmRatio = elm.width() / elm.height();
+                        if( ratio <= elmRatio){
+                            elm.css('height', containerHeight)
+                            .css({'left': -(elm.width() - containerWidth) / 2,
+                                    'min-height': '100%',
+                                    'width' : 'auto'
+                                });
+                        }else if (ratio > elmRatio ){
+                           elm.css('width', containerWidth)
+                           .css({ 'top': -(elm.height() - containerHeight) / 2,
+                                    'min-width': '100%',
+                                    'height' : 'auto' 
+                                });
+                        }
+                    if(pos == 0){
+                        elm.addClass(settings.elmName).css({
+                            'position':'absolute',
+                        });
+                    }else{
+                        elm.addClass(settings.elmName).css({
+                            'position':'absolute',
+                            'opacity' : '0'
+                        });
+                    }
+                }).each(function(){
+                    if(this.complete){
+                        $(this).trigger('load');
                     }
                 });
-                setInterval(function(){
-                    self.children(":first")
-                        .fadeOut(settings.fadeSpeed)
-                        .next('img')
-                        .fadeIn(settings.fadeSpeed)
-                        .end()
-                        .appendTo('.' + settings.parentName);
-                },settings.duration); 
             });
+            setInterval(function(){
+                self.children(":first")
+                    .animate({'opacity':0},settings.fadeSpeed)
+                    .next('img')
+                    .animate({'opacity':100},settings.fadeSpeed)
+                    .end()
+                    .appendTo('#' + elmID);
+            },settings.duration); 
         });
         return this;
     }
