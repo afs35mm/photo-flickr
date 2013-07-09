569a70cbfd7bae23efbcfebd14e17ebd3909b7dc
AS, akqa <afs35mm@gmail.com>
2013-07-09 15:21:38 -0400

    changes to plugin for updated plugin pattern

--- js/EzFade.js
+++ js/EzFade.js
@@ -1,6 +1,19 @@
-(function($) {
-    $.fn.EzFade = function(options) {
-        var settings = $.extend({
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = "EzFade",
+        defaults = {
             duration: 3000,
             parentName: 'EzFade',
             elmName: 'EzFadeElm',
@@ -8,45 +21,80 @@
             width: '100%',
             height: '100%',
             position: 'relative'
-        }, options);        
-        $(this).each(function(){
-            var self = $(this),
-                elmID = self.attr('id');
-            self.css({
-                'width' : settings.width,
-                'height' : settings.height,
-                'position': settings.position, 
+        };
+
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = $(element);
+        self = this;
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend( {}, defaults, options );
+
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.dimensions = {};
+        this.childDimensions = {};
+        
+        this.init();
+
+        console.log(this);
+        
+        $(window).resize(function(){
+            self.getDimensions(self.element);
+            self.getChildrenSize(self.element);
+        });
+    }
+
+    Plugin.prototype = {
+
+        init: function() {
+            // Place initialization logic here
+            // You already have access to the DOM element and
+            // the options via the instance, e.g. this.element
+            // and this.options
+            // you can add more functions like the one below and
+            // call them like so: this.yourOtherFunction(this.element, this.options).
+       
+            this.element.addClass('defaults.parentName').css({
+                'width' : defaults.width,
+                'height' : defaults.height,
+                'position': defaults.position, 
                 'overflow': 'hidden'
-            }).addClass(settings.parentName);
-            var containerHeight = self.height(),
-                containerWidth = self.width();
-                ratio = containerWidth / containerHeight;
-            self.children().each(function(pos,value){
-                $(this).load(function(){
-                    var elm = $(this);
-                        var elmRatio = elm.width() / elm.height();
-                        if( ratio <= elmRatio){
-                            elm.css('height', containerHeight)
-                            .css({'left': -(elm.width() - containerWidth) / 2,
-                                    'min-height': '100%',
-                                    'width' : 'auto'
-                                });
-                        }else if (ratio > elmRatio ){
-                           elm.css('width', containerWidth)
-                           .css({ 'top': -(elm.height() - containerHeight) / 2,
-                                    'min-width': '100%',
-                                    'height' : 'auto' 
-                                });
-                        }
-                    if(pos == 0){
-                        elm.addClass(settings.elmName).css({
-                            'position':'absolute',
-                        });
-                    }else{
-                        elm.addClass(settings.elmName).css({
-                            'position':'absolute',
-                            'opacity' : '0'
-                        });
+            }).children().css({
+                'position': 'absolute'
+            });
+
+            this.getDimensions(this.element);
+            this.getChildrenSize(this.element);
+
+            // this.getContainerSize(this.element);
+            // this.getChildrenSize(this.element);
+        },
+
+        getDimensions: function(el){
+            this.dimensions.width = el.width();
+            this.dimensions.height = el.height();
+            this.dimensions.ratio = (this.dimensions.width / this.dimensions.height);
+        },
+
+        getChildrenSize : function(el){
+            
+            var parent = this;
+
+            el.children().each(function(key,val){
+                var elm = $(val);
+                elm.load(function(){
+                    var elmRatio = elm.width() / elm.height();
+                    if(elmRatio <= parent.dimensions.ratio){
+                        elm.css('width', parent.dimensions.width)
+                        .css('top', (-( elm.height() - parent.dimensions.height) / 2));
+                    }else if (elmRatio > parent.dimensions.ratio){
+                         elm.css('height', parent.dimensions.height)
+                        .css('left', (-( elm.width() - parent.dimensions.width) / 2));
                     }
                 }).each(function(){
                     if(this.complete){
@@ -54,15 +102,19 @@
                     }
                 });
             });
-            setInterval(function(){
-                self.children(":first")
-                    .animate({'opacity':0},settings.fadeSpeed)
-                    .next('img')
-                    .animate({'opacity':100},settings.fadeSpeed)
-                    .end()
-                    .appendTo('#' + elmID);
-            },settings.duration); 
+        }
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+            }
         });
-        return this;
-    }
-}(jQuery));
+    };
+
+
+
+})( jQuery, window, document );
