ef8d14ed475793406bf8ea33f6a5a0d862b75925
AS, akqa <afs35mm@gmail.com>
2013-07-10 16:08:55 -0400

    refactoring slideshow

--- js/EzFade.js
+++ js/EzFade.js
@@ -34,14 +34,14 @@
         this.options = $.extend( {}, defaults, options );
         this._defaults = defaults;
         this._name = pluginName;        
-        this.dimensions = {};
+        this.windowDimensions = {};
         this.childDimensions = {};
         
         this.init();
+        console.log(this);
 
         $(window).resize(function(){
-            self.getDimensions(self.element);
-            self.getChildrenSize(self.element);
+            
         });
     }
 
@@ -54,73 +54,103 @@
             // and this.options
             // you can add more functions like the one below and
             // call them like so: this.yourOtherFunction(this.element, this.options).
-       
             this.element.addClass(defaults.parentName).css({
                 'width' : defaults.width,
                 'height' : defaults.height,
                 'position': defaults.position, 
                 'overflow': 'hidden'
             }).children().addClass(defaults.childName).css({
-                'position': 'absolute',
-                'visibility' : 'hidden'
+                'position': 'absolute'
 
             });
 
-            this.getDimensions(this.element);
-            this.getChildrenSize(this.element);
-            this.startFade(this.element);
+            this.getWindowDimensions(this);
 
-            // this.getContainerSize(this.element);
-            // this.getChildrenSize(this.element);
-        },
-
-        startFade: function(el){
-            var elmID = $(el).attr('id');
-            console.log(el);
-            setInterval(function(){
-            console.log(elmID);
-                $(el).children(":first")
-                    .animate({'opacity':0},defaults.fadeSpeed)
-                    .next('img')
-                    .animate({'opacity':100},defaults.fadeSpeed)
-                    .end()
-                    .appendTo('#' + elmID);
-            },defaults.duration); 
-        },
-
-        getDimensions: function(el){
-            this.dimensions.width = el.width();
-            this.dimensions.height = el.height();
-            this.dimensions.ratio = (this.dimensions.width / this.dimensions.height);
-        },
-
-        getChildrenSize : function(el){
-            var parent = this;
-            el.children().each(function(key,val){
-                var elm = $(val);
-                elm.load(function(){
-                    var elmRatio = elm.width() / elm.height();
-                    if(elmRatio <= parent.dimensions.ratio){
-                        elm.css({
-                            'width': parent.dimensions.width,
-                            'visibility' : 'visible' 
-                        })
-                        .css('top', (-( elm.height() - parent.dimensions.height) / 2));
-                    }else if (elmRatio > parent.dimensions.ratio){
-                         elm.css('height', parent.dimensions.height)
-                        .css('left', (-( elm.width() - parent.dimensions.width) / 2));
-                    }
-                    if(key != 0){
-                        elm.css('opacity',0);
-                    } 
-                }).each(function(){
-                    if(this.complete){
-                        $(this).trigger('load');
-                    }
+            $(self.element).children().each(function(key,val){
+                var _val = $(val);
+                _val.load(function(){
+                    var imgWidth = _val.width()
+                        imgHeight = _val.height(),
+                        imgRatio = imgWidth / imgHeight;
+                        self.childDimensions[key] = {};
+                        self.childDimensions[key].w = imgWidth;
+                        self.childDimensions[key].h = imgHeight;
+                        self.childDimensions[key].ratio = imgRatio;
+                        //console.log(self.childDimensions[key].ratio );
+                        self.resize(self.windowDimensions.ratio, self.childDimensions[key].ratio , _val)
                 });
             });
-        }
+        },
+
+        getWindowDimensions: function(obj){
+            obj.windowDimensions.w = obj.element.width();
+            obj.windowDimensions.h = obj.element.height();
+            obj.windowDimensions.ratio = obj.windowDimensions.w /obj.windowDimensions.h;
+            console.log(obj.windowDimensions);
+        },
+
+        resize: function(windowRatio, childRatio, imgToResize){
+            if(childRatio <= windowRatio){
+                imgToResize.css({
+                    'width': this.windowDimensions.w
+                })
+                .css('top', (-( imgToResize.height() - this.windowDimensions.h) / 2));
+            }else if (childRatio > windowRatio){
+                imgToResize.css({
+                    'height': this.windowDimensions.h
+                })
+                .css('left', (-( imgToResize.width() - this.windowDimensions.w) / 2));
+            }
+        }   
     };
+
+    //     startFade: function(el){
+    //         var elmID = $(el).attr('id');
+    //         console.log(el);
+    //         setInterval(function(){
+    //         console.log(elmID);
+    //             $(el).children(":first")
+    //                 .animate({'opacity':0},defaults.fadeSpeed)
+    //                 .next('img')
+    //                 .animate({'opacity':100},defaults.fadeSpeed)
+    //                 .end()
+    //                 .appendTo('#' + elmID);
+    //         },defaults.duration); 
+    //     },
+
+    //     getDimensions: function(el){
+    //         this.dimensions.width = el.width();
+    //         this.dimensions.height = el.height();
+    //         this.dimensions.ratio = (this.dimensions.width / this.dimensions.height);
+    //     },
+
+    //     getChildrenSize : function(el){
+    //         var parent = this;
+    //         el.children().each(function(key,val){
+    //             var elm = $(val);
+    //             elm.load(function(){
+    //                 var elmRatio = elm.width() / elm.height();
+    //                 if(elmRatio <= parent.dimensions.ratio){
+    //                     elm.css({
+    //                         'width': parent.dimensions.width,
+    //                         'visibility' : 'visible' 
+    //                     })
+    //                     .css('top', (-( elm.height() - parent.dimensions.height) / 2));
+    //                 }else if (elmRatio > parent.dimensions.ratio){
+    //                      elm.css('height', parent.dimensions.height)
+    //                     .css('left', (-( elm.width() - parent.dimensions.width) / 2));
+    //                 }
+    //                 if(key != 0){
+    //                     elm.css('opacity',0);
+    //                 } 
+    //             }).each(function(){
+    //                 if(this.complete){
+    //                     $(this).trigger('load');
+    //                 }
+    //             });
+    //         });
+    //     }
+    //};
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
